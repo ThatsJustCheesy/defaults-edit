66fb553c6ea2203c396ec3341652c5d34c9c1d69
Ian Gregory <justcheesy@outlook.com>
2020-06-06 18:18:57 -0400

    Use Data+FastHex via SwiftPM instead of git submodules

---
+++ defaults-edit/FastHexTransformer.swift
@@ -0,0 +1,25 @@
+//
+//  FastHexTransformer.swift
+//  defaults-edit
+//
+//  Created by Ian Gregory on 6 Jun ’20.
+//  Copyright © 2020 Ian Gregory. All rights reserved.
+//
+
+import Foundation
+import Data_FastHex
+
+// TODO: Convert to an NSFormatter?
+
+@objc(FastHexTransformer)
+class FastHexTransformer: ValueTransformer {
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        (value as? NSData)?.lowercaseHexStringRepresentation ?? ""
+    }
+    
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        NSData(hexString: value as! String)
+    }
+    
+}
